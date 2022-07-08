eb8078d6139ee9c206bfd005cf6212fa73ec7027
Mia Schroeder <miaschroeder527@gmail.com>
2022-07-08 03:20:56 -0700

    Installed axios dependency

---
+++ src/components/suggestedClothesComponent/SuggestedClothesComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import { Card, List } from 'antd';
+import SuggestedTopsComponent from '../suggestedTopsComponent/SuggestedTopsComponent';
+
+const SuggestedClothesComponent = () => {
+
+    return (
+        <SuggestedTopsComponent />
+    );
+}
+
+export default SuggestedClothesComponent;
